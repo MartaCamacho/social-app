69520cc8dfd9eabfdeab188a3a980be43e8e390a
martacamacho <martacamachovargas@gmail.com>
2021-01-11 23:47:14 +0100

    delete button half working, single comment detail working

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import AuthRoute from './util/AuthRoute'
 import MenuBar from './components/MenuBar';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import SinglePost from './pages/SinglePost';
 import Register from './pages/Register';
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
           <Route exact path='/' component={Home}/>
           <AuthRoute exact path='/login' component={Login}/>
           <AuthRoute exact path='/register' component={Register}/>
+          <Route exact path='/posts/:postId' component={SinglePost}/>
         </Container>
       </Router>
     </AuthProvider>
